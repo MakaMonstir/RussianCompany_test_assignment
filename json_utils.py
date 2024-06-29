c9c874c820c87e76892c9b0f90bf015cddf00331
Timur Usmanov <fonuatimur@gmail.com>
2024-06-29 14:58:59 +0700

    add default serializer for convertion to json

--- json_utils.py
+++ json_utils.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
 import json
 import logging
-from typing import Dict, List
+from typing import Dict, List, Optional
+from datetime import date
 
 def save_data_to_json(data: List[Dict], file_path):
     """
@@ -11,7 +13,19 @@ def save_data_to_json(data: List[Dict], file_path):
     """
     try:
         with open(file_path, 'w', encoding="utf-8") as jsonfile:
-            json.dump(data, jsonfile, ensure_ascii=False, indent=None)
+            json.dump(data, jsonfile, ensure_ascii=False, indent=None, default=default_serializer)
         logging.info(f"Данные загружены в {file_path}")
     except IOError as error:
         logging.error("Ошибка при записи в файл: {error}")
+
+def default_serializer(object: Optional[date|Decimal]) -> str:
+    """
+    Сериализатор для объектов с поддержкой типов, не встроенных в json сериализатор.
+    """
+    if isinstance(object, date):
+        return object.strftime("%m/%d/%Y, %H:%M:%S")
+    
+    if isinstance(object, Decimal):
+        return float(object)
+    
+    return str(object)
